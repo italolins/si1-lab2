fb986c4c688ecb3b5a0890d4e3a6176c32fc2fda
italolins <italo.lins@ccc.ufcg.edu.br>
2014-06-14 12:20:56 -0300

    modificado

---
+++ si1-lab1/README.txt
@@ -0,0 +1,2 @@
+Este repositório tem o código
+do lab1 de SI1 modificado
---
+++ si1-lab1/dinamica/javascript/script.js
@@ -0,0 +1,7 @@
+
+function handle(e){
+	if(e.keyCode == 13){
+		alert("Enter was pressed was presses")
+	}
+	return false;
+}
